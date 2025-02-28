804115e866ad59f7d1fd844c47b853b5ebd0243a
AlicjaDev <AlicjaDev@users.noreply.github.com>
2025-02-28 13:59:05 +0000

    deploy: 87b662c4ae705b656f118894076768d8b1c069f9

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,36 +1,63 @@
-const CACHE_NAME = 'to-do-pwa-cache-v1'; 
+const CACHE_NAME = 'planner-pwa-cache-v1';
 const FILES_TO_CACHE = [
     '/https://github.com/AlicjaDev/Planner/',
-    '/https://github.com/AlicjaDev/Planner/index.html', 
-    '/https://github.com/AlicjaDev/Planner/style.css', 
+    '/https://github.com/AlicjaDev/Planner/index.html',
+    '/https://github.com/AlicjaDev/Planner/style.css',
+    '/https://github.com/AlicjaDev/Planner/calendar.html',
     'https://github.com/AlicjaDev/Planner/app.js',
     'https://github.com/AlicjaDev/Planner/index.js',
-    ];
+];
 
-    self.addEventListener('install', (event) => { 
-        event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then((cache) => cache.addAll(FILES_TO_CACHE))
-     );
-    });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+});
 
-    
-    self.addEventListener('fetch', (event) => { event.respondWith(
-    caches.match(event.request)
-    .then((response) => response || fetch(event.request))
-    
+
+self.addEventListener('fetch', (event) => {
+
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.hostname.includes('firestore.googleapis.com')) {
+        return fetch(event.request);
+    }
+
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+
 
     );
-    });
-    
+});
 
-// const sw = new URL('service-worker.js', import.meta.url)
-// if ('serviceWorker' in navigator) {
-//  const s = navigator.serviceWorker;
-//  s.register(sw.href, {
-//  scope: 'https://alicjadev.github.io/Planner/'
-//  })
-//  .then(_ => console.log('Service Worker Registered for scope:', sw.href,
-// 'with', import.meta.url))
-//  .catch(err => console.error('Service Worker Error:', err));
-// }
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function (registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(function (error) {
+            console.error('Service Worker registration failed:', error);
+        });
+}
+
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
